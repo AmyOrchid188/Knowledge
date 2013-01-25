f116f4754b359424d0f9714d252f78dda1ca24ff
AmyOrchid188 <liujunhui08@gmail.com>
2013-01-25 10:53:35 +0800

    add menu and infinite loop shell script

---
+++ bash_tutorial/infinite_loop.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Recommand syntax for setting an infinite while loop
+#while :
+#do
+#   echo "Do something; hit[CTRL-C] to stop!"
+#done
+
+# use true command to set an infinite while loop
+#while true
+#do
+#   echo "Do something; hit[CTRL-C] to stop!"
+#done
+# use false command to set an infinite while loop
+while false 
+do
+   echo "Do something; hit[CTRL-C] to stop!"
+done
