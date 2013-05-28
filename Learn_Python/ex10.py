5009e5425b429627ac5f581f6053f70f324c4b34
AmyOrchid188 <liujunhui08@gmail.com>
2013-05-28 09:41:40 +0800

    add ex9 to ex13 files.

---
+++ Learn_Python/ex10.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+# Author: Junhui Liu <liujunhui08@gmail.com>
+# Created Time: 05/28/13 09:02:25 (CST)
+# Modified Time: 05/28/13 09:05:50 (CST)
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishes
+\t* Catnip\n\t* Grass
+"""
+
+print tabby_cat
+
+print persian_cat
+print backslash_cat
+print fat_cat
