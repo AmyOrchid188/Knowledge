e62b02ad5991c81571da183adadf5bfeb8b6e72c
AmyOrchid188 <liujunhui08@gmail.com>
2013-06-20 18:21:38 +0800

    first change appy to pass parameter through browser address

--- Learn_Python/projects/gothonweb/bin/app.py
+++ Learn_Python/projects/gothonweb/bin/app.py
@@ -2,11 +2,11 @@
 #-*- coding:utf-8 -*-
 # Author: Junhui Liu <liujunhui08@gmail.com>
 # Created Time: 06/20/13 08:33:23 (CST)
-# Modified Time: 06/20/13 08:49:05 (CST)
+# Modified Time: 06/20/13 18:19:03 (CST)
 import web
 
 urls = (
-    '/', 'Index'
+    '/hello', 'Index'
 )
 
 app = web.application(urls, globals())
@@ -15,7 +15,12 @@ render = web.template.render('templates/')
 
 class Index(object):
     def GET(self):
-        greeting = "Helllo World"
-        return render.index(greeting= greeting)
+        form =  web.input(name="Nobody", greet=None)
+        if form.greet:
+            greeting = "%s, %s" % (form.greet, form.name)
+            return render.index(greeting=greeting)
+        else:
+            return "Error, greet is required."
+
 if __name__ == "__main__":
     app.run()
