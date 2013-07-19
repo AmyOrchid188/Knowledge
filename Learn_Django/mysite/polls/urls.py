3396ff6a4b14f184c6621d47f7919ef6a9fab6fa
AmyOrchid188 <liujunhui08@gmail.com>
2013-07-19 08:55:48 +0800

    finish fill a context and return an httpresponse object.

---
+++ Learn_Django/mysite/polls/urls.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+# Author: Junhui Liu <liujunhui08@gmail.com>
+# Created Time: 07/18/13 18:22:06 (CST)
+# Modified Time: 07/18/13 18:39:14 (CST)
+
+from django.conf.urls import patterns, url
+
+from polls import views
+
+urlpatterns = patterns('',
+                       # ex: /polls/
+                       url(r'^$', views.index, name='index'),
+                       # ex: /polls/5/
+                       url(r'^(?P<poll_id>\d+)/$',views.detail, name='detail'),
+                       # ex: /polls/5/results/
+                       url(r'^(?P<poll_id>\d+)/results/$',views.results, name='results'),
+                       # ex:/polls/5/vote/
+                       url(r'^(?P<poll_id>\d+)/vote/$', views.vote, name='vote'),
+                       )
--- Learn_Django/mysite/polls/views.py
+++ Learn_Django/mysite/polls/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import RequestContext, loader
 
+from polls.models import Poll
+
+from django.http import HttpResponse
 # Create your views here.
+def index(request):
+    latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = RequestContext(request, {
+        'latest_poll_list': latest_poll_list,
+    })
+    return HttpResponse(template.render(context))
+    #return HttpResponse("Hello, world. You're at the poll index.")
+
+def detail(request, poll_id):
+    return HttpResponse("You're looking at the details of poll  %s." % poll_id)
+
+def results(request, poll_id):
+    return HttpResponse("You're looking at the results of poll %s." % poll_id)
+
+def vote(request, poll_id):
+    return HttpResponse("You're voting on poll %s." % poll_id)
